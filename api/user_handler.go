8b87e4ef95bbd16b472f78461ee774b6024551fc
abdddev <abdddev@gmail.com>
2025-12-21 19:40:32 +1000

    add user validation

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,8 +1,6 @@
 package api
 
 import (
-	"context"
-
 	"github.com/abdddev/hotel-reservation/db"
 	"github.com/abdddev/hotel-reservation/types"
 	"github.com/gofiber/fiber/v2"
@@ -18,12 +16,30 @@ func NewUserHandler(userStore db.UserStore) *UserHandler {
 	}
 }
 
+func (h *UserHandler) HandlerPostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	if errors := params.Validate(); len(errors) > 0 {
+		return c.JSON(errors)
+	}
+	user, err := types.NewUserFromParams(params)
+	if err != nil {
+		return err
+	}
+	insertedUser, err := h.userStore.InsertUser(c.Context(), user)
+	if err != nil {
+		return err
+	}
+	return c.JSON(insertedUser)
+}
+
 func (h *UserHandler) HandlerGetUser(c *fiber.Ctx) error {
 	var (
-		id  = c.Params("id")
-		ctx = context.Background()
+		id = c.Params("id")
 	)
-	user, err := h.userStore.GetUserById(ctx, id)
+	user, err := h.userStore.GetUserById(c.Context(), id)
 	if err != nil {
 		return err
 	}
@@ -31,9 +47,9 @@ func (h *UserHandler) HandlerGetUser(c *fiber.Ctx) error {
 }
 
 func (h *UserHandler) HandlerGetUsers(c *fiber.Ctx) error {
-	u := types.User{
-		FirstName: "James",
-		LastName:  "At the watercooler",
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
 	}
-	return c.JSON(u)
+	return c.JSON(users)
 }
--- types/user.go
+++ types/user.go
@@ -1,7 +1,66 @@
 package types
 
+import (
+	"fmt"
+	"regexp"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	bcryptCost      = 12
+	minFirstNameLen = 2
+	minLastNameLen  = 2
+	minPasswordLen  = 7
+)
+
+type CreateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func (p CreateUserParams) Validate() []string {
+	errors := []string{}
+	if len(p.FirstName) < minFirstNameLen {
+		errors = append(errors, fmt.Sprintf("firstName length should be at least %d characters", minFirstNameLen))
+	}
+	if len(p.LastName) < minLastNameLen {
+		errors = append(errors, fmt.Sprintf("lastName length should be at least %d characters", minLastNameLen))
+	}
+	if len(p.Password) < minPasswordLen {
+		errors = append(errors, fmt.Sprintf("password length should be at least %d characters", minPasswordLen))
+	}
+	if !isEmailValid(p.Email) {
+		errors = append(errors, fmt.Sprintf("email is invalid"))
+	}
+	return errors
+}
+
+func isEmailValid(email string) bool {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+	return emailRegex.MatchString(email)
+}
+
 type User struct {
-	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string `bson:"firstName" json:"firstName"`
-	LastName  string `bson:"lastName" json:"lastName"`
+	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName         string             `bson:"firstName" json:"firstName"`
+	LastName          string             `bson:"lastName" json:"lastName"`
+	Email             string             `bson:"email" json:"email"`
+	EncryptedPassword string             `bson:"EncryptedPassword" json:"-"`
+}
+
+func NewUserFromParams(params CreateUserParams) (*User, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcryptCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		FirstName:         params.FirstName,
+		LastName:          params.LastName,
+		Email:             params.Email,
+		EncryptedPassword: string(encpw),
+	}, nil
 }
